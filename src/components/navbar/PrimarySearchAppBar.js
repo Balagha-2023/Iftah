86d7bf1b0f2feb8047a8a7d22d17a2c839ff560d
mn-48 <nazmul.cse48@gmail.com>
2023-12-13 19:48:59 +0600

    IFtah

--- src/components/navbar/PrimarySearchAppBar.js
+++ src/components/navbar/PrimarySearchAppBar.js
@@ -173,7 +173,7 @@ export default function PrimarySearchAppBar() {
             component="div"
             sx={{ display: { xs: 'none', sm: 'block' } }}
           >
-            Iftah
+           IFTAH
           </Typography>
           <Search>
             <SearchIconWrapper>
