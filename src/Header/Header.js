60eb2cd60fa684b5bb0e021a8b3f11ecca1d5ee6
mn-48 <nazmul.cse48@gmail.com>
2023-12-13 21:01:40 +0600

    Header Init

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import {AppBar, Toolbar, Box} from "@mui/material";
+import {AppBar, Toolbar, Tabs, Box, Tab} from "@mui/material";
 import ApiIcon from '@mui/icons-material/Api';
+
+
+const links = ['Iftah','Product', 'Solution', 'Pricing', 'Enterprice'] 
 
 const Header = () => {
   return <AppBar sx={{bgcolor:"transparent", boxShadow:0}}>
     <Toolbar>
-      <Box >
+      <Box sx={{display: "flex", width: "100%"}}>
         <ApiIcon sx={{color: "black"}} />
+        <Box>
+          
+          <Tabs>
+            {links.map((link, index) => (<Tab key={index} label={link} />) )}
+          </Tabs>
+              
+        </Box>
       </Box>
 
     </Toolbar>
